acc0adf1871eab3185cced3d39664d1d5ba66fd0
mikhi-mh <hmikhilesh@gmail.com>
2023-04-30 18:01:06 +0530

    Initial Commit

---
+++ src/main/java/com/mikhi/pingapp/PingAppApplication.java
@@ -0,0 +1,13 @@
+package com.mikhi.pingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikhi/pingapp/PingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhi.pingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
